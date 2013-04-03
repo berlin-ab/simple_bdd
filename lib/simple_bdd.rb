fe993e3a1e8dfb033aa02364ed42f56b67bf8d70
Adam Berlin and Ryan McGarvey <pair+adam+ryan@pivotallabs.com>
2013-04-03 18:17:28 -0400

    Output method definition when it is missing.  (a la cucumber).

--- lib/simple_bdd.rb
+++ lib/simple_bdd.rb
@@ -1,12 +1,18 @@
 require "simple_bdd/version"
 
 module SimpleBdd
-
   ["given", "when", "then", "and", "Given", "When", "Then", "And", "also", "Also"].each do |method|
     define_method(method) do |message|
-      method = message.downcase.gsub(" ", "_").gsub(/\W/, "")
-      send(method)
+      begin
+        send(translate_message(message))
+      rescue NoMethodError
+        STDOUT.puts("def #{translate_message(message)}\n\nend")
+        fail
+      end
+    end
+
+    def translate_message(message)
+      message.downcase.gsub(" ", "_").gsub(/\W/, "")
     end
   end
-
 end
